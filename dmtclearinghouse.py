d693f4f2414ac4cfec068543a1b4088f3bb40bf3
hbarrett <hays.barrett@gmail.com>
2020-03-10 13:29:00 -0600

    Added some facets

--- dmtclearinghouse.py
+++ dmtclearinghouse.py
@@ -27,7 +27,7 @@ Users =Base.classes.users
 Taxonomies = Base.classes.taxonomies
 
 resources_facets = ["facet_author_org", "facet_subject", "facet_keywords", "facet_license", "facet_usage_rights", "facet_publisher", "facet_access_features",
-                    "facet_language_primary", "facet_languages_secondary", "facet_ed_framework", "facet_target_audience", "facet_type", "facet_purpose", "facet_media_type"]
+                    "facet_language_primary", "facet_languages_secondary", "facet_ed_frameworks", "facet_target_audience", "facet_type", "facet_purpose", "facet_media_type"]
 # Create a pysolr object for accessing the "learningresources" and "users" index
 resources = pysolr.Solr(
     app.config["SOLR_ADDRESS"]+"learningresources/", timeout=10)
