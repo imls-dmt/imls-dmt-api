02b1ca89b4d16298346e353e33691325bb290434
Hays Barrett <hbarrett@esip-dev-02.edac.unm.edu>
2020-06-22 09:25:42 -0600

    Fixed nested resoure facets.

--- dmtclearinghouse.py
+++ dmtclearinghouse.py
@@ -42,8 +42,8 @@ Users =Base.classes.users
 Taxonomies = Base.classes.taxonomies
 Tokens = Base.classes.tokens
 
-resources_facets = ["facet_author_org", "facet_subject", "facet_keywords", "facet_license", "facet_usage_info", "facet_publisher", "facet_access_features",
-                    "facet_language_primary", "facet_languages_secondary", "facet_ed_frameworks", "facet_target_audience", "facet_lr_type", "facet_purpose", "facet_media_type"]
+resources_facets = ["facet_author_org", "facet_subject", "facet_keywords", "facet_license", "facet_usage_info", "facet_publisher",
+                    "facet_accessibility_features.name","facet_language_primary", "facet_languages_secondary", "facet_ed_frameworks", "facet_target_audience", "facet_lr_type", "facet_purpose", "facet_media_type"]
 # Create a pysolr object for accessing the "learningresources" and "users" index
 resources = pysolr.Solr(
     app.config["SOLR_ADDRESS"]+"learningresources/", timeout=10)
